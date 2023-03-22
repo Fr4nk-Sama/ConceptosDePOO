e5a7c94f92e0a0a7c008f512bc3fb22d0c7d66ea
Fr4nk-Sama <l19111772@itcj.edu.mx>
2023-03-21 23:01:16 -0600

    Date class, employee and salary completed.

---
+++ ConceptosPOO/ConceptosPOO/Program.cs
@@ -0,0 +1,17 @@
+﻿using ConceptosPOO;
+
+Console.WriteLine("Hello, Robal!");
+
+Date date1 = new Date(2023, 03, 21);
+
+try
+{
+    Console.WriteLine(date1.ToString());
+    Console.WriteLine(new Date(2023, 11, 45));
+    Console.WriteLine(new Date(2023, 19, 21));
+}
+catch (Exception error)
+{
+
+    Console.WriteLine(error.Message);
+}
